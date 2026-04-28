7c6dd3534a1d92a96c5003fcf8a4602a1256668e
James Holloway <james.holloway94@gmail.com>
2026-04-28 17:04:36 +0000

    refresh aws config sync marker after nightly reconcile

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-28 16:05:12 UTC
+# Last sync: 2026-04-28 17:04:36 UTC
