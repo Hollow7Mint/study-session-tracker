6920b893adae5b451c2e6bfc0fca87a0534838e9
James Holloway <james.holloway94@gmail.com>
2026-04-29 11:29:39 +0000

    preserve nat gateway sync time in infra banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-29 10:33:01 UTC
+# Last sync: 2026-04-29 11:29:39 UTC
