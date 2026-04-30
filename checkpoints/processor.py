0217ead1ca5d0d90f1415085731ecfaeab7a48c0
James Holloway <james.holloway94@gmail.com>
2026-04-30 11:13:58 +0000

    write bastion access log time into metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-30 10:11:04 UTC
+# Last sync: 2026-04-30 11:13:58 UTC
