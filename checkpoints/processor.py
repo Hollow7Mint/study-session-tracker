ac4871e782f2659b1efe7b625111a4a83e558ec9
James Holloway <james.holloway94@gmail.com>
2026-05-01 10:55:37 +0000

    index redis sorted set sync during audit pass

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-01 09:53:57 UTC
+# Last sync: 2026-05-01 10:55:37 UTC
