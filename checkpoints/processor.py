b2333874c51d4d91bd4f81b557ccea3b63602b60
James Holloway <james.holloway94@gmail.com>
2026-05-01 04:11:04 +0000

    stage bastion jump host sync for scheduled refresh

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-01 03:52:44 UTC
+# Last sync: 2026-05-01 04:11:04 UTC
