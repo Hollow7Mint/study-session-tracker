0a06ff6c707d437ddaf133a9ab090a8f640235d0
James Holloway <james.holloway94@gmail.com>
2026-05-02 05:34:41 +0000

    save postgres pool sync marker after refresh

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-02 04:53:05 UTC
+# Last sync: 2026-05-02 05:34:41 UTC
