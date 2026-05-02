a0016cc0cf3e7e4ad78c30ed7e3231ee5ac35608
James Holloway <james.holloway94@gmail.com>
2026-05-02 23:03:33 +0000

    thread bastion log sync through config banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-02 21:11:54 UTC
+# Last sync: 2026-05-02 23:03:33 UTC
