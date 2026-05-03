3990e361f4e219e58d35757beb3bbbdff817af34
James Holloway <james.holloway94@gmail.com>
2026-05-03 19:48:54 +0000

    finalize vpc security group sync after rule push

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-03 18:20:06 UTC
+# Last sync: 2026-05-03 19:48:54 UTC
