0692128726f24e8443331ae662c4950d3bb397bd
James Holloway <james.holloway94@gmail.com>
2026-05-04 18:05:28 +0000

    advance ssh key trust sync into config record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-04 16:55:08 UTC
+# Last sync: 2026-05-04 18:05:28 UTC
