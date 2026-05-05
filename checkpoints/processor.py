a774e7394525f18ba74af33165fb17b11fb58294
James Holloway <james.holloway94@gmail.com>
2026-05-05 10:37:26 +0000

    restore mysql binlog time for replication audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-05 09:19:47 UTC
+# Last sync: 2026-05-05 10:37:26 UTC
