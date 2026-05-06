80be1bb612db9cc52ee6840dfb22a603297f9a13
James Holloway <james.holloway94@gmail.com>
2026-05-06 18:27:22 +0000

    lift asg capacity sync time into deploy banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-06 17:12:17 UTC
+# Last sync: 2026-05-06 18:27:22 UTC
