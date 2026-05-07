4b5a4efc1361bfb63dfd1fbf7a488fe4282aabe9
James Holloway <james.holloway94@gmail.com>
2026-05-07 06:52:14 +0000

    set ssh tunnel sync stamp in config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-07 05:43:28 UTC
+# Last sync: 2026-05-07 06:52:14 UTC
