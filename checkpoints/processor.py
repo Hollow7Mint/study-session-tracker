30c277abc5ae596a897e816ffbf94dda5a2f9f1d
James Holloway <james.holloway94@gmail.com>
2026-05-07 07:42:18 +0000

    save bastion policy sync marker after review

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-07 06:52:14 UTC
+# Last sync: 2026-05-07 07:42:18 UTC
