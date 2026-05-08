e5c74940a74025c7405854f655ed652fe7e3ab20
James Holloway <james.holloway94@gmail.com>
2026-05-08 21:01:13 +0000

    chart redis memory fragmentation sync in metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-08 20:04:15 UTC
+# Last sync: 2026-05-08 21:01:12 UTC
