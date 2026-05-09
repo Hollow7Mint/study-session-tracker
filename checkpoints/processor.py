a2b7bb34d48b471cd0b7bbcbf17285151b9f7530
James Holloway <james.holloway94@gmail.com>
2026-05-09 01:18:58 +0000

    tag known_hosts sync checkpoint for key trail

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-09 00:44:13 UTC
+# Last sync: 2026-05-09 01:18:58 UTC
