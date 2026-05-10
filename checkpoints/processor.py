d515a4bb573640ad6bf6cfea699fe4a86c70ba49
James Holloway <james.holloway94@gmail.com>
2026-05-10 15:48:02 +0000

    restore ssh public key time for rotation audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-10 14:52:28 UTC
+# Last sync: 2026-05-10 15:48:02 UTC
