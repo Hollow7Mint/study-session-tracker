742a116c2dccd99c78a44d48e5fc3f176ba00d62
James Holloway <james.holloway94@gmail.com>
2026-05-11 20:27:12 +0000

    persist vpc route table sync after update

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 19:56:24 UTC
+# Last sync: 2026-05-11 20:27:12 UTC
