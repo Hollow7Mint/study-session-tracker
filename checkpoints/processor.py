699b1e3b2efe4cc17b29580b75bb4b52929eda32
James Holloway <james.holloway94@gmail.com>
2026-05-11 15:13:39 +0000

    remap ec2 security group rule time in config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 14:15:09 UTC
+# Last sync: 2026-05-11 15:13:39 UTC
