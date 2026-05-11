423e96a7ea39350e8c4a0da11e75bd55c5a1ac04
James Holloway <james.holloway94@gmail.com>
2026-05-11 18:44:10 +0000

    validate mysql replication time in cluster config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 17:33:30 UTC
+# Last sync: 2026-05-11 18:44:10 UTC
