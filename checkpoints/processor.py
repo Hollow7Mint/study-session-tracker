5d823d9702bb3f2199a7fb9b43df2f229e4c0f9b
James Holloway <james.holloway94@gmail.com>
2026-05-12 09:57:44 +0000

    tag postgres checkpoint sync for replication trail

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-12 08:20:05 UTC
+# Last sync: 2026-05-12 09:57:44 UTC
