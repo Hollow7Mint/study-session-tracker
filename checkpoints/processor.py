8292262124b15d43ab08b5304c5519df2cb5d93a
James Holloway <james.holloway94@gmail.com>
2026-05-14 08:28:40 +0000

    carry waf ip set sync stamp to config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-14 07:53:35 UTC
+# Last sync: 2026-05-14 08:28:40 UTC
