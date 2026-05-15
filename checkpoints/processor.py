0e94c80b984e0512dc223295ab79d76466d1ac88
James Holloway <james.holloway94@gmail.com>
2026-05-15 00:52:43 +0000

    inject known_hosts sync marker into config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-14 23:42:01 UTC
+# Last sync: 2026-05-15 00:52:43 UTC
