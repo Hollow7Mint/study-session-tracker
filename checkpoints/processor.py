363f4833ca348d9b288b12aaebca5f2c1341ba3f
James Holloway <james.holloway94@gmail.com>
2026-05-15 14:59:44 +0000

    annotate ec2 ami bake time for deploy record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-15 14:01:59 UTC
+# Last sync: 2026-05-15 14:59:44 UTC
