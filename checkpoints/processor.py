3a548f40a321a28e80cf5c4f3af7a5f5529bc9bd
James Holloway <james.holloway94@gmail.com>
2026-05-15 22:42:35 +0000

    anchor cloudwatch alarm threshold time in config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-15 21:35:56 UTC
+# Last sync: 2026-05-15 22:42:35 UTC
