b6514703ea8d0877f49430bd5c8af8dac40432f0
James Holloway <james.holloway94@gmail.com>
2026-05-15 15:14:57 +0000

    merge ssh trust anchor time into metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-15 14:59:44 UTC
+# Last sync: 2026-05-15 15:14:57 UTC
