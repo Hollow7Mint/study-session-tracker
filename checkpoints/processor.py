8fe0c3a87b9a40314e334975057ee072fff14554
James Holloway <james.holloway94@gmail.com>
2026-05-18 01:15:44 +0000

    audit known_hosts validation time in record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-18 00:25:21 UTC
+# Last sync: 2026-05-18 01:15:44 UTC
