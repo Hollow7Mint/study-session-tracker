0037b2f8a233fb88e2d659df903aab0be3197ae7
James Holloway <james.holloway94@gmail.com>
2026-05-19 23:08:17 +0000

    prime redis memory sync marker after flush

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-19 23:02:27 UTC
+# Last sync: 2026-05-19 23:08:17 UTC
