caebf11e6e070f0bd43deaf5a223bbf2e365f864
James Holloway <james.holloway94@gmail.com>
2026-05-20 19:50:09 +0000

    reload elb health check marker for nightly audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-20 18:54:33 UTC
+# Last sync: 2026-05-20 19:50:09 UTC
