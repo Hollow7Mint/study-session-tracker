cdc14a543aa5aea72eead9027522c684772d6f91
James Holloway <james.holloway94@gmail.com>
2026-05-20 06:12:44 +0000

    store ssh revocation list sync for key audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-20 05:33:34 UTC
+# Last sync: 2026-05-20 06:12:44 UTC
