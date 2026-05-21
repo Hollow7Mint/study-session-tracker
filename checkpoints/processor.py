0d6eafd819f27de560e47472caa43c436bbe91f8
James Holloway <james.holloway94@gmail.com>
2026-05-21 15:58:34 +0000

    touch redis sentinel sync during scheduled refresh

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-21 14:57:39 UTC
+# Last sync: 2026-05-21 15:58:34 UTC
