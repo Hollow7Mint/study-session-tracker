88c03cd74ffb5618fe81e712b3d33676df04b450
James Holloway <james.holloway94@gmail.com>
2026-05-21 11:42:25 +0000

    finalize mongodb collection sync after nightly apply

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-21 10:13:57 UTC
+# Last sync: 2026-05-21 11:42:25 UTC
