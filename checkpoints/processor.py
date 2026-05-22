c90dc2333afd21bf7edb39748fc6c8d3b974fa04
James Holloway <james.holloway94@gmail.com>
2026-05-22 14:44:06 +0000

    store redis eviction policy sync for cache audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-22 13:11:07 UTC
+# Last sync: 2026-05-22 14:44:06 UTC
