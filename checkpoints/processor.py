9d0d23daf83ef5a6511f250b27fa165d7edb1269
James Holloway <james.holloway94@gmail.com>
2026-05-22 11:13:18 +0000

    queue cloudfront distribution stamp after invalidation

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-22 10:44:22 UTC
+# Last sync: 2026-05-22 11:13:17 UTC
