28b164fe5921bf096772f6125775eee13cdaf0f6
James Holloway <james.holloway94@gmail.com>
2026-05-23 23:57:34 +0000

    mirror ssh certificate chain time in record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-23 22:31:40 UTC
+# Last sync: 2026-05-23 23:57:34 UTC
