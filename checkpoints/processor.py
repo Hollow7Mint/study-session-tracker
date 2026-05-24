224570c9c9ebf61e4078818a9769d7da027c8131
James Holloway <james.holloway94@gmail.com>
2026-05-24 15:23:15 +0000

    frame ssm document sync time in config record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-24 14:26:34 UTC
+# Last sync: 2026-05-24 15:23:15 UTC
