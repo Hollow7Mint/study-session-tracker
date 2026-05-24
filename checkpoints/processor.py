9d3150f6707685ccb5cb38f2a589288933ae86cc
James Holloway <james.holloway94@gmail.com>
2026-05-24 04:45:09 +0000

    post postgres replication heartbeat to config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-24 03:36:48 UTC
+# Last sync: 2026-05-24 04:45:09 UTC
