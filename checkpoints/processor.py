dd5b1e2f3356084ead52daabe18144f9989c6c6e
James Holloway <james.holloway94@gmail.com>
2026-05-24 20:18:36 +0000

    stamp inspector scan completion in security record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-24 19:30:24 UTC
+# Last sync: 2026-05-24 20:18:36 UTC
