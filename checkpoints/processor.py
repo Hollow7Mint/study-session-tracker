400a07871f53dee2beed42bc49f226cf399c52d3
James Holloway <james.holloway94@gmail.com>
2026-05-24 18:12:30 +0000

    normalize ecs cluster config time in metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-24 17:27:02 UTC
+# Last sync: 2026-05-24 18:12:30 UTC
