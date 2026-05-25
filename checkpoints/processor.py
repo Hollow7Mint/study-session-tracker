aeb7d3c0f9359a7f7df845842e65f8ca9854c26e
James Holloway <james.holloway94@gmail.com>
2026-05-25 03:47:27 +0000

    emit redis cluster heartbeat for scheduled audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-25 03:03:44 UTC
+# Last sync: 2026-05-25 03:47:27 UTC
