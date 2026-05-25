0b279b4ff38f39ea932abc0089281311fff748ac
James Holloway <james.holloway94@gmail.com>
2026-05-25 22:08:37 +0000

    pin mongodb shard balancing time in config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-25 21:08:24 UTC
+# Last sync: 2026-05-25 22:08:37 UTC
