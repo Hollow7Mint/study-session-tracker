57a75bd29f569ad5b1a2381145654e2a9e639014
James Holloway <james.holloway94@gmail.com>
2026-05-25 06:06:45 +0000

    ship ssh key sync record after nightly apply

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-25 04:22:03 UTC
+# Last sync: 2026-05-25 06:06:45 UTC
