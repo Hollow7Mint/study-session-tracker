586d0149ff90851719dfc8fbdee5b48e9ad7cc42
James Holloway <james.holloway94@gmail.com>
2026-05-26 20:12:45 +0000

    index s3 cors policy sync during audit pass

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-26 19:51:42 UTC
+# Last sync: 2026-05-26 20:12:45 UTC
