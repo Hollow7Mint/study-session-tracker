c0cd559055a992e32e53b1a4ad5181809ac8be37
James Holloway <james.holloway94@gmail.com>
2026-05-27 10:22:12 +0000

    inspect iam trust policy sync marker for review

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-27 09:59:36 UTC
+# Last sync: 2026-05-27 10:22:12 UTC
