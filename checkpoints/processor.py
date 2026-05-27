35eb4199c6f6e9d4e3cefca5d997f1e745710259
James Holloway <james.holloway94@gmail.com>
2026-05-27 15:06:42 +0000

    store cloudfront ttl sync stamp for cache audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-27 13:52:36 UTC
+# Last sync: 2026-05-27 15:06:42 UTC
