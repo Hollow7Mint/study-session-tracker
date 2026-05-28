fd306522d76803be593097c6eadddaa10389037a
James Holloway <james.holloway94@gmail.com>
2026-05-28 18:30:41 +0000

    lift ssh certificate expiry time into banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-28 17:44:14 UTC
+# Last sync: 2026-05-28 18:30:41 UTC
