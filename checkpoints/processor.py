a5574af625b913c39720f7af6263e4692e112d1a
James Holloway <james.holloway94@gmail.com>
2026-05-28 13:00:33 +0000

    advance mysql gtid sync into config record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-28 11:35:51 UTC
+# Last sync: 2026-05-28 13:00:33 UTC
