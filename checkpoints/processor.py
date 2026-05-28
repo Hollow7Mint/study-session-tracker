9137a43edc2f7d4ee9177e00c2b32347d4c54d7c
James Holloway <james.holloway94@gmail.com>
2026-05-28 10:43:01 +0000

    retain ssh key expiry stamp for rotation audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-28 09:32:26 UTC
+# Last sync: 2026-05-28 10:43:01 UTC
