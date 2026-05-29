4f53a249c2813355d3116f98bd95a649850f3be2
James Holloway <james.holloway94@gmail.com>
2026-05-29 01:46:08 +0000

    resync parameter store path time for nightly audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-29 00:58:56 UTC
+# Last sync: 2026-05-29 01:46:08 UTC
