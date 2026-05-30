fff21c15cf7e840e21497fbf179e2a8b346703ee
James Holloway <james.holloway94@gmail.com>
2026-05-30 17:08:34 +0000

    harvest postgres vacuum sync for nightly audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-30 17:04:58 UTC
+# Last sync: 2026-05-30 17:08:34 UTC
