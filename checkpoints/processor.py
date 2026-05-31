db7c0c351404b4a8d7cfdc2747b2fd2cfa3a8ba8
James Holloway <james.holloway94@gmail.com>
2026-05-31 11:18:32 +0000

    surface codebuild log sync time for pipeline audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-31 10:18:03 UTC
+# Last sync: 2026-05-31 11:18:30 UTC
