b682f910e3ae59b4563785cf852be093beb12f27
James Holloway <james.holloway94@gmail.com>
2026-05-31 09:43:49 +0000

    chart autoscale sync time in deployment metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-31 08:17:40 UTC
+# Last sync: 2026-05-31 09:43:49 UTC
