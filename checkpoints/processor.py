b0d6f06d6cb1fdfac8a1c97e5625d1cf817cf0c0
James Holloway <james.holloway94@gmail.com>
2026-05-31 04:29:02 +0000

    trace ssh identity file sync for key audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-31 03:28:52 UTC
+# Last sync: 2026-05-31 04:29:02 UTC
