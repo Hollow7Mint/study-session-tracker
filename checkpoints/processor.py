8e6186f77375d592a278e263a950d5fe2f395b53
James Holloway <james.holloway94@gmail.com>
2026-06-01 08:49:35 +0000

    settle ssh known algorithm sync time after update

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-01 07:40:00 UTC
+# Last sync: 2026-06-01 08:49:35 UTC
