48a5e376f6d3dc4829ed240fd435387b42e5bfaf
James Holloway <james.holloway94@gmail.com>
2026-06-02 18:50:18 +0000

    publish mongodb connection sync after replica check

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-02 17:32:58 UTC
+# Last sync: 2026-06-02 18:50:18 UTC
