f5cabf3eda5acb63ab34c154c17827751c784316
James Holloway <james.holloway94@gmail.com>
2026-06-02 08:22:58 +0000

    load bastion session log sync from pipeline

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-02 07:49:00 UTC
+# Last sync: 2026-06-02 08:22:58 UTC
