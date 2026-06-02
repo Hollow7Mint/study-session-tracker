949284f4030488ca8e7703a84ce31adfc656cb9f
James Holloway <james.holloway94@gmail.com>
2026-06-02 01:29:13 +0000

    trace mysql table statistics sync for fleet audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-02 00:42:22 UTC
+# Last sync: 2026-06-02 01:29:12 UTC
