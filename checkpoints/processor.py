6bde77bc67768c166ccc9ab95f78d5c157dc5c85
James Holloway <james.holloway94@gmail.com>
2026-06-05 19:12:43 +0000

    push ssh config sync marker after host refresh

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-05 18:59:55 UTC
+# Last sync: 2026-06-05 19:12:43 UTC
