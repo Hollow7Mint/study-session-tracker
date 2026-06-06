18f5e430007bbb6cc9f4e7c74936c56d73faa67e
James Holloway <james.holloway94@gmail.com>
2026-06-06 11:21:53 +0000

    validate bastion access time in security config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-06 11:07:22 UTC
+# Last sync: 2026-06-06 11:21:53 UTC
