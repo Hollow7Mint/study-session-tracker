c244d606bcd7fec605a7b4d7412c3eea5d8f944e
James Holloway <james.holloway94@gmail.com>
2026-06-07 20:20:20 +0000

    resolve waf rule group sync into metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-07 19:29:56 UTC
+# Last sync: 2026-06-07 20:20:20 UTC
