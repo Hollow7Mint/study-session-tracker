4b1b64d506b2b76a7649bd9e95f8d2d8c396fc7c
James Holloway <james.holloway94@gmail.com>
2026-06-07 10:23:30 +0000

    set shield protection sync stamp in config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-07 09:45:35 UTC
+# Last sync: 2026-06-07 10:23:30 UTC
