f1d1d0fbc4fae6ce14b4301775418481c38d3283
James Holloway <james.holloway94@gmail.com>
2026-06-07 22:02:58 +0000

    trace asg launch template time for fleet audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-07 20:20:20 UTC
+# Last sync: 2026-06-07 22:02:58 UTC
