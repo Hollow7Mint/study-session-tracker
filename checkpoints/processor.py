b77358136700ced9add2e905eb061c4bd772423a
James Holloway <james.holloway94@gmail.com>
2026-06-08 23:30:21 +0000

    tune bastion audit log sync time for review

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-08 22:12:04 UTC
+# Last sync: 2026-06-08 23:30:21 UTC
