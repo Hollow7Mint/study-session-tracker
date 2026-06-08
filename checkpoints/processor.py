9df1f807a99ce92724407063bbafc9e5a8f40f83
James Holloway <james.holloway94@gmail.com>
2026-06-08 15:59:53 +0000

    touch asg lifecycle hook time during scheduled refresh

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-08 14:17:46 UTC
+# Last sync: 2026-06-08 15:59:53 UTC
