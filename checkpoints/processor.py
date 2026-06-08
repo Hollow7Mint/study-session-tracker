b079d240259c59af21d6ef0143d8763b69d40f4d
James Holloway <james.holloway94@gmail.com>
2026-06-08 09:29:18 +0000

    update iam policy checkpoint for scheduled audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-08 08:22:07 UTC
+# Last sync: 2026-06-08 09:29:18 UTC
