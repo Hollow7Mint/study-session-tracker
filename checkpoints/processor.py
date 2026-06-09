7ec75057e6de6a95f8cb83c21bdbd8c558c74e88
James Holloway <james.holloway94@gmail.com>
2026-06-09 17:01:16 +0000

    post cdk bootstrap sync stamp to config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-09 16:00:24 UTC
+# Last sync: 2026-06-09 17:01:16 UTC
