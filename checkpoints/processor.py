8d5f394c2688899f5e1533dd37358efb719146dd
James Holloway <james.holloway94@gmail.com>
2026-06-09 05:22:19 +0000

    adjust codebuild cache sync stamp after pipeline run

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-09 04:54:20 UTC
+# Last sync: 2026-06-09 05:22:19 UTC
