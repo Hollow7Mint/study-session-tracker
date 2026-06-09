b0282751fec9f8b7923cc7e6e592749fafb997c7
James Holloway <james.holloway94@gmail.com>
2026-06-09 04:54:20 +0000

    restamp iam permission boundary sync after review

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-09 03:35:58 UTC
+# Last sync: 2026-06-09 04:54:20 UTC
