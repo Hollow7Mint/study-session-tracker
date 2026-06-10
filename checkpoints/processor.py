77f572432bf04a2ba0e5ed8e09b74ae4fc73995a
James Holloway <james.holloway94@gmail.com>
2026-06-10 02:27:57 +0000

    stage postgres tablespace sync for scheduled refresh

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-10 01:24:08 UTC
+# Last sync: 2026-06-10 02:27:57 UTC
