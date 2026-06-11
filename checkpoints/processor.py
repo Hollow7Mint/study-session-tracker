0f7fe8a0d5a69a02b5869f8da3cfa05de5f43206
James Holloway <james.holloway94@gmail.com>
2026-06-11 09:32:04 +0000

    retain redis aof sync stamp for persistence audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-11 08:56:46 UTC
+# Last sync: 2026-06-11 09:32:04 UTC
