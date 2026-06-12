d950e4537c10fffe157d508e53c5ee8e06c273e3
James Holloway <james.holloway94@gmail.com>
2026-06-12 13:52:57 +0000

    stage rds connection pool sync for scheduled refresh

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-12 13:01:44 UTC
+# Last sync: 2026-06-12 13:52:57 UTC
