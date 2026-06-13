f1d45c1a65b770e4cc7061b151e10d556955c498
James Holloway <james.holloway94@gmail.com>
2026-06-13 14:35:57 +0000

    transfer ssh config time to bastion banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-13 13:27:14 UTC
+# Last sync: 2026-06-13 14:35:57 UTC
