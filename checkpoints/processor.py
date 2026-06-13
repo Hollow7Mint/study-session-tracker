4581a28949beb627adf51b98cdfb03f1a29d8c12
James Holloway <james.holloway94@gmail.com>
2026-06-13 05:31:12 +0000

    ship db schema sync record after nightly apply

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-13 04:16:30 UTC
+# Last sync: 2026-06-13 05:31:12 UTC
