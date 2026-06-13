133731baafe146b56e0266c67826aeda4e423ffd
James Holloway <james.holloway94@gmail.com>
2026-06-13 17:21:54 +0000

    snapshot iam role binding time for audit trail

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-13 16:50:46 UTC
+# Last sync: 2026-06-13 17:21:54 UTC
