0fd1d94d05dfaff94d3658a652411c5a5849f9b2
James Holloway <james.holloway94@gmail.com>
2026-06-13 16:50:46 +0000

    catalog rds parameter group sync after maintenance

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-13 15:42:01 UTC
+# Last sync: 2026-06-13 16:50:46 UTC
