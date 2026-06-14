2011a04b59f57e8b4ba8e2be5d278f89fc416e0a
James Holloway <james.holloway94@gmail.com>
2026-06-14 00:24:11 +0000

    persist connection string refresh in config banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-13 23:26:19 UTC
+# Last sync: 2026-06-14 00:24:11 UTC
