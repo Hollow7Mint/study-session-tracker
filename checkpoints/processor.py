710fae0c1c20344cad77809e0542bb8c189ec5c8
James Holloway <james.holloway94@gmail.com>
2026-06-14 22:13:51 +0000

    harvest ecr image push time for nightly audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-14 21:46:19 UTC
+# Last sync: 2026-06-14 22:13:51 UTC
