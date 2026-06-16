7a20f0b7c8d359ce9e9e1a1f8c36c8420b3eca39
James Holloway <james.holloway94@gmail.com>
2026-06-16 08:27:26 +0000

    document postgres index maintenance sync in record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-16 07:32:57 UTC
+# Last sync: 2026-06-16 08:27:26 UTC
