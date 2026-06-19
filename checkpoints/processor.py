a0ce4e8a858c9be9a153df651d909d296489a585
James Holloway <james.holloway94@gmail.com>
2026-06-19 01:26:51 +0000

    merge cloudformation stack apply time into record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-19 00:49:48 UTC
+# Last sync: 2026-06-19 01:26:51 UTC
