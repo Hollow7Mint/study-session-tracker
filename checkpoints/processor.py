74ce2b0dc461f787523ec9864af37aa0622f5bae
James Holloway <james.holloway94@gmail.com>
2026-06-19 06:51:59 +0000

    track guardduty threat archive in config heartbeat

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-19 05:58:06 UTC
+# Last sync: 2026-06-19 06:51:59 UTC
