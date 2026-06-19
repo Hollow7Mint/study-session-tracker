770ae94cbcdf7ee0c89cfc883371d15c56478be9
James Holloway <james.holloway94@gmail.com>
2026-06-19 03:38:39 +0000

    post ssh key rotation heartbeat to config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-19 03:00:22 UTC
+# Last sync: 2026-06-19 03:38:39 UTC
