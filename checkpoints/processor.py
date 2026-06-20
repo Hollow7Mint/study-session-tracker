0ec2146320ec3df5bd91f16facf81e8e0eba642f
James Holloway <james.holloway94@gmail.com>
2026-06-20 12:45:31 +0000

    snapshot bastion session time for security audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-20 11:32:24 UTC
+# Last sync: 2026-06-20 12:45:31 UTC
