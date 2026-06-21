932c98ca5c26d12353075d6f2c9424e5fa720c26
James Holloway <james.holloway94@gmail.com>
2026-06-21 07:33:53 +0000

    adjust redis ttl sync stamp after cache flush

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-21 06:45:10 UTC
+# Last sync: 2026-06-21 07:33:53 UTC
