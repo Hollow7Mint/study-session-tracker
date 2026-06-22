5f5727fc5878d4a16a6df1da18f148370e5c48eb
James Holloway <james.holloway94@gmail.com>
2026-06-22 14:57:53 +0000

    align vpc subnet allocation time in config banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-22 14:07:14 UTC
+# Last sync: 2026-06-22 14:57:53 UTC
