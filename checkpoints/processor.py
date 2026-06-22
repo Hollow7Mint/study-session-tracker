d9ade95dff4a97da6908bd929e46440806c164ea
James Holloway <james.holloway94@gmail.com>
2026-06-22 14:07:14 +0000

    annotate mysql slow log sync time for audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-22 12:38:07 UTC
+# Last sync: 2026-06-22 14:07:14 UTC
