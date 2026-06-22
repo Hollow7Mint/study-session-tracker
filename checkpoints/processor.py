be94a1ff0be1f0fb73f93739f188d3175977bedb
James Holloway <james.holloway94@gmail.com>
2026-06-22 09:03:06 +0000

    merge mongodb shard sync time into metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-22 07:16:47 UTC
+# Last sync: 2026-06-22 09:03:06 UTC
