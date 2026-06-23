af10d01be75238ae2277ac8fe964d27841a8bb6a
James Holloway <james.holloway94@gmail.com>
2026-06-23 01:32:49 +0000

    resolve mysql backup sync into metadata file

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-23 00:27:11 UTC
+# Last sync: 2026-06-23 01:32:49 UTC
