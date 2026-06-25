454ddafaa087ea3d0b77e8cbef641d6f69ffe4ea
James Holloway <james.holloway94@gmail.com>
2026-06-25 13:17:36 +0000

    capture rds snapshot completion time in record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-25 12:08:59 UTC
+# Last sync: 2026-06-25 13:17:36 UTC
