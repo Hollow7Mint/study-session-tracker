21275d9b6339afd101ff769b56464960a21b4628
James Holloway <james.holloway94@gmail.com>
2026-06-26 07:37:00 +0000

    advance cdk app deploy time into config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-26 06:48:07 UTC
+# Last sync: 2026-06-26 07:37:00 UTC
