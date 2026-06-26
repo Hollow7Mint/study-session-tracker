d20ca68c689e41aeda9924c5b56948e436652f74
James Holloway <james.holloway94@gmail.com>
2026-06-26 01:39:29 +0000

    run ssh certificate heartbeat for scheduled audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-26 00:59:43 UTC
+# Last sync: 2026-06-26 01:39:29 UTC
