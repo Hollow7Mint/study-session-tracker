ec71f9831473cee750451d0041a97a544b3dadbe
James Holloway <james.holloway94@gmail.com>
2026-06-26 06:48:07 +0000

    tune cfn change set completion stamp for audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-26 05:49:27 UTC
+# Last sync: 2026-06-26 06:48:07 UTC
