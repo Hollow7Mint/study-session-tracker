334fbe0b5b2e7b7d60d83e87341f0be66bb19071
James Holloway <james.holloway94@gmail.com>
2026-06-28 15:48:02 +0000

    relay codebuild artifact time through config banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-28 14:55:14 UTC
+# Last sync: 2026-06-28 15:48:02 UTC
