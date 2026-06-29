4f704437b5f405f82519365060249c9e4b3412b0
James Holloway <james.holloway94@gmail.com>
2026-06-29 06:35:42 +0000

    preserve mysql user permission sync in metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-29 06:08:46 UTC
+# Last sync: 2026-06-29 06:35:42 UTC
