9361ead2fbac46c0e3edbf7bc8a1e321bfdfc506
James Holloway <james.holloway94@gmail.com>
2026-06-29 23:47:04 +0000

    mirror guardduty archive time in metadata record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-29 22:44:20 UTC
+# Last sync: 2026-06-29 23:47:04 UTC
