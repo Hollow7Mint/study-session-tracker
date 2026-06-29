a73d2527c92e78e7bba5978c8c4d01f6be38e6b8
James Holloway <james.holloway94@gmail.com>
2026-06-29 13:36:51 +0000

    inject ecs task definition marker into config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-29 13:06:43 UTC
+# Last sync: 2026-06-29 13:36:51 UTC
