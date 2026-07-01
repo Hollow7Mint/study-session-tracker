559f308a2a1b784d741497fb730fecce8806ef7b
James Holloway <james.holloway94@gmail.com>
2026-07-01 00:39:48 +0000

    inspect postgres lock wait sync for performance

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-30 23:45:35 UTC
+# Last sync: 2026-07-01 00:39:48 UTC
