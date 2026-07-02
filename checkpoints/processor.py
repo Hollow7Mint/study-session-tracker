1251eae0f65947e9d146e1f42c75e20b6fb4396c
James Holloway <james.holloway94@gmail.com>
2026-07-02 23:50:34 +0000

    persist tunnel config refresh in ssh banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-02 23:06:39 UTC
+# Last sync: 2026-07-02 23:50:34 UTC
