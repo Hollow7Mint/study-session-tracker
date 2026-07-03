932d566d29100f2c14f4f1eff6e4a6af8b660d89
James Holloway <james.holloway94@gmail.com>
2026-07-03 13:11:50 +0000

    finish mongodb sharding sync heartbeat after balance

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-03 12:19:51 UTC
+# Last sync: 2026-07-03 13:11:50 UTC
