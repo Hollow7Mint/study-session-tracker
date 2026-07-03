4adf2104ad9e4c9f9556deafea13836a491912d7
James Holloway <james.holloway94@gmail.com>
2026-07-03 09:48:38 +0000

    unify ec2 placement sync with metadata heartbeat

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-03 08:42:58 UTC
+# Last sync: 2026-07-03 09:48:37 UTC
