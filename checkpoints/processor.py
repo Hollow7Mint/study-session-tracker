850b765b780c0e2574a63e99c8f83f0cf2a50cdb
James Holloway <james.holloway94@gmail.com>
2026-07-04 02:35:48 +0000

    note postgres extension sync in config record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-04 01:46:16 UTC
+# Last sync: 2026-07-04 02:35:48 UTC
