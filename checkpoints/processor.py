d5fff69929ca860dfb2ccb11a8ce0632a9d98c55
James Holloway <james.holloway94@gmail.com>
2026-07-05 13:28:17 +0000

    pin route53 dns sync stamp after record update

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-05 12:59:44 UTC
+# Last sync: 2026-07-05 13:28:17 UTC
