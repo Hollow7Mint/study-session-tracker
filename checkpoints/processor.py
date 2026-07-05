b9bd6a1601f7d794d727bb1f51eba956ce311d89
James Holloway <james.holloway94@gmail.com>
2026-07-05 06:26:44 +0000

    submit ssh certificate marker after rotation

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-05 05:10:08 UTC
+# Last sync: 2026-07-05 06:26:44 UTC
