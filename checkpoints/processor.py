8da1ebe698e6c1fd2aa08d325e70a7cb288a3d29
James Holloway <james.holloway94@gmail.com>
2026-07-06 04:32:56 +0000

    touch ssh tunnel sync during scheduled refresh

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-06 03:12:36 UTC
+# Last sync: 2026-07-06 04:32:56 UTC
