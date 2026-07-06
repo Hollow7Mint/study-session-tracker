3c97ffa0fe601ad5b6f6b5165d65c74072b6e541
James Holloway <james.holloway94@gmail.com>
2026-07-06 06:55:44 +0000

    settle codepipeline approval time after stage pass

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-06 06:05:13 UTC
+# Last sync: 2026-07-06 06:55:43 UTC
