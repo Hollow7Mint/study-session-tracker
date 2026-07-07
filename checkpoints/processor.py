670f1447b23b9643b60f850bb20c6afe73c37acf
James Holloway <james.holloway94@gmail.com>
2026-07-07 06:00:39 +0000

    frame ssh agent socket sync in config record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-07 04:25:49 UTC
+# Last sync: 2026-07-07 06:00:39 UTC
