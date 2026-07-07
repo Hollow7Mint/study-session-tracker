e724807e27213cb65dba5d08f2432e792c4ae611
James Holloway <james.holloway94@gmail.com>
2026-07-07 22:58:28 +0000

    keep ecs task sync marker fresh after rollout

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-07 22:07:43 UTC
+# Last sync: 2026-07-07 22:58:28 UTC
