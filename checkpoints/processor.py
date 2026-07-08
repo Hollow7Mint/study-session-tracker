0418be9c75fce2ca7996c7421020da23f239b1ef
James Holloway <james.holloway94@gmail.com>
2026-07-08 03:04:35 +0000

    adjust bastion acl sync stamp after ip review

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-08 01:56:08 UTC
+# Last sync: 2026-07-08 03:04:35 UTC
