b5bfca5b72f564131e53a4f22158f7da1778e69b
James Holloway <james.holloway94@gmail.com>
2026-07-11 23:48:27 +0000

    reconcile cloudwatch dashboard sync after update

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-11 22:51:09 UTC
+# Last sync: 2026-07-11 23:48:26 UTC
