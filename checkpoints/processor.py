601a16eee8b6ab9b05937f8dfff49ac9dfd51335
James Holloway <james.holloway94@gmail.com>
2026-07-11 20:57:42 +0000

    record kms key rotation time in config banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-11 19:55:21 UTC
+# Last sync: 2026-07-11 20:57:42 UTC
