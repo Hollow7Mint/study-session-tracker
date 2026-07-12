53307516c1d7c3360bebc40f3285578f2efb0458
James Holloway <james.holloway94@gmail.com>
2026-07-12 22:58:45 +0000

    push mysql connection pool sync after refresh

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-12 21:22:58 UTC
+# Last sync: 2026-07-12 22:58:45 UTC
