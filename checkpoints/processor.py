45592877499205042ec2b7f7e6c6a0425585e9df
James Holloway <james.holloway94@gmail.com>
2026-07-13 20:19:07 +0000

    flush ec2 fleet inventory marker after scan

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-13 19:21:42 UTC
+# Last sync: 2026-07-13 20:19:07 UTC
