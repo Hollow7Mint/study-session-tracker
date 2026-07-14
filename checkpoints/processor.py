b8cee96ad521f31983a5e3e6b8f9e80c94f0f494
James Holloway <james.holloway94@gmail.com>
2026-07-14 03:40:20 +0000

    set redis cluster sync stamp in config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-14 03:03:29 UTC
+# Last sync: 2026-07-14 03:40:20 UTC
