64b28a763e73404518ba0fd805f7e905519b116e
James Holloway <james.holloway94@gmail.com>
2026-07-14 20:19:31 +0000

    rotate kms key metadata sync after policy apply

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-14 19:11:54 UTC
+# Last sync: 2026-07-14 20:19:31 UTC
