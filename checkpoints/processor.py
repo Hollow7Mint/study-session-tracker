6ee72a6f81fa9106dcda9a2ac9269cbea011f872
James Holloway <james.holloway94@gmail.com>
2026-07-15 21:01:36 +0000

    close post-deploy heartbeat for infra pipeline

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-15 19:55:31 UTC
+# Last sync: 2026-07-15 21:01:36 UTC
