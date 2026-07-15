c8c6e258044098f8979d4d1ed0ce0d98edb84320
James Holloway <james.holloway94@gmail.com>
2026-07-15 02:43:52 +0000

    bridge lambda env var sync through config banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-15 01:14:05 UTC
+# Last sync: 2026-07-15 02:43:52 UTC
