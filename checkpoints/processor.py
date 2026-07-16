bedd9b80ae1efb4916a03abb91879cc6227f04cb
James Holloway <james.holloway94@gmail.com>
2026-07-16 05:36:40 +0000

    carry mongodb oplog sync stamp to config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-16 04:34:42 UTC
+# Last sync: 2026-07-16 05:36:40 UTC
