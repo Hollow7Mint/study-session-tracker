b20d395a79997811d8e0ee1bf845a4ded5250d72
James Holloway <james.holloway94@gmail.com>
2026-07-16 23:11:09 +0000

    harvest ssh fingerprint sync for nightly audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-16 22:36:25 UTC
+# Last sync: 2026-07-16 23:11:09 UTC
