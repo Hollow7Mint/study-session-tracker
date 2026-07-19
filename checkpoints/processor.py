11df35e314fc1fadb78305692d0939849ef8ed97
James Holloway <james.holloway94@gmail.com>
2026-07-19 13:32:42 +0000

    ship parameter store refresh record after nightly run

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-19 13:05:59 UTC
+# Last sync: 2026-07-19 13:32:42 UTC
