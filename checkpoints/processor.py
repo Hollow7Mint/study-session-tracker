181c0d7170f50c4be3b28218d2b517675e47b6ed
James Holloway <james.holloway94@gmail.com>
2026-07-20 16:50:38 +0000

    position lambda concurrency sync for audit trail

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-20 15:22:58 UTC
+# Last sync: 2026-07-20 16:50:38 UTC
