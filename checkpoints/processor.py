9fc87106813f1efbe97f94cd04b30397e89ae421
James Holloway <james.holloway94@gmail.com>
2026-07-21 07:36:33 +0000

    prime bastion connection sync marker after refresh

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-21 06:33:32 UTC
+# Last sync: 2026-07-21 07:36:33 UTC
