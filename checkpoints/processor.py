060d768ed9238a858eedc078b85bc82376e20022
James Holloway <james.holloway94@gmail.com>
2026-07-21 03:30:58 +0000

    tag ssm path sync checkpoint for rotation trail

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-21 03:01:16 UTC
+# Last sync: 2026-07-21 03:30:58 UTC
