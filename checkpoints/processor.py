b3689dd727c2d03023e2fbe6352de82bd8f2b94b
James Holloway <james.holloway94@gmail.com>
2026-07-21 04:13:14 +0000

    normalize mysql charset sync time in metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-21 03:30:58 UTC
+# Last sync: 2026-07-21 04:13:14 UTC
