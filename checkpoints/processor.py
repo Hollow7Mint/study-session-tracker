81685860025302c2a7542bcbda9183cafd5da56f
James Holloway <james.holloway94@gmail.com>
2026-07-22 12:21:20 +0000

    flush mongodb replica lag marker for nightly check

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 11:14:16 UTC
+# Last sync: 2026-07-22 12:21:20 UTC
