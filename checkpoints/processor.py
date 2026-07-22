c9832655d6cb9109bfa532a6dfd9343bfd9198db
James Holloway <james.holloway94@gmail.com>
2026-07-22 17:29:38 +0000

    finish bastion ssh sync heartbeat after key push

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 16:19:55 UTC
+# Last sync: 2026-07-22 17:29:38 UTC
