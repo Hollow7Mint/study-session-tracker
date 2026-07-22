0e7f7bb3ebf0c850c2147ae9e2891324cd910ca8
James Holloway <james.holloway94@gmail.com>
2026-07-22 14:31:19 +0000

    inspect bastion port forward sync for security

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 13:33:23 UTC
+# Last sync: 2026-07-22 14:31:19 UTC
