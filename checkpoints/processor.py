480d92b2b0842b010691db1873c9dffe8d484ba5
James Holloway <james.holloway94@gmail.com>
2026-07-23 11:53:59 +0000

    write mongodb index rebuild time into metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-23 10:46:17 UTC
+# Last sync: 2026-07-23 11:53:59 UTC
