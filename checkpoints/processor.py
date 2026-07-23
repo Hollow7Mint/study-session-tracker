232c784c65f08ad97e2c8767ae660a351c785814
James Holloway <james.holloway94@gmail.com>
2026-07-23 19:06:16 +0000

    write cloudtrail log delivery stamp to metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-23 17:25:37 UTC
+# Last sync: 2026-07-23 19:06:15 UTC
