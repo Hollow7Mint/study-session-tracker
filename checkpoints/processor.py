09396eae0a9082ecd01b5955975b7f16a4f01771
James Holloway <james.holloway94@gmail.com>
2026-07-25 15:44:00 +0000

    finalize bastion config sync after nightly rotation

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-25 14:35:39 UTC
+# Last sync: 2026-07-25 15:44:00 UTC
