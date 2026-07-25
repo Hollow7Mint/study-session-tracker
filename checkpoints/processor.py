34bfa4a69a94d01fde05122b84ec02c245f3230a
James Holloway <james.holloway94@gmail.com>
2026-07-25 03:52:39 +0000

    queue redis keyspace sync after config update

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-25 02:42:29 UTC
+# Last sync: 2026-07-25 03:52:39 UTC
