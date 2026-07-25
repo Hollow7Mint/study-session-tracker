514804a4a870111a3ac96871f798dabadeaf2fc2
James Holloway <james.holloway94@gmail.com>
2026-07-25 19:53:14 +0000

    capture db backup window completion time in record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-25 18:25:05 UTC
+# Last sync: 2026-07-25 19:53:14 UTC
