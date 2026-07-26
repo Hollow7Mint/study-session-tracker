9a33b0b668dd12a8e7596c53dc0bcf84871b7128
James Holloway <james.holloway94@gmail.com>
2026-07-26 09:27:03 +0000

    tune mongodb replication lag time for audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-26 08:52:20 UTC
+# Last sync: 2026-07-26 09:27:03 UTC
