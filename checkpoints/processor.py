9c6070baa7b1e782b4ed18140804cb7884468f91
James Holloway <james.holloway94@gmail.com>
2026-07-27 21:03:43 +0000

    anchor ssh client config sync after key rotation

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-27 19:45:40 UTC
+# Last sync: 2026-07-27 21:03:43 UTC
