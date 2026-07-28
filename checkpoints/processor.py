2941c06785a8c48b1e28be49033f72da08a7af8d
James Holloway <james.holloway94@gmail.com>
2026-07-28 15:52:07 +0000

    bump known_hosts update stamp after rotation

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-28 14:16:36 UTC
+# Last sync: 2026-07-28 15:52:07 UTC
