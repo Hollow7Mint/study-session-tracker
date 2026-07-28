877fdd58921c7c2f8d379364df9279a9eb4c18a9
James Holloway <james.holloway94@gmail.com>
2026-07-28 05:47:55 +0000

    rebuild nat gateway sync record after route change

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-28 05:09:17 UTC
+# Last sync: 2026-07-28 05:47:55 UTC
