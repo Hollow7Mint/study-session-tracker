32081880a41409d1141a715f309df51a791d6242
James Holloway <james.holloway94@gmail.com>
2026-07-31 10:41:42 +0000

    resolve known_hosts cleanup sync into metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-31 09:59:29 UTC
+# Last sync: 2026-07-31 10:41:42 UTC
