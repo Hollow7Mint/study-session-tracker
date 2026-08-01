548bb1add7b0a57496daf56caa1e5044896e9e49
James Holloway <james.holloway94@gmail.com>
2026-08-01 04:27:40 +0000

    index bastion user sync during audit pass

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-01 03:24:25 UTC
+# Last sync: 2026-08-01 04:27:40 UTC
