d63b3cd44a899a34b3db51921088b39e3e3b4d2e
James Holloway <james.holloway94@gmail.com>
2026-08-03 09:56:26 +0000

    transfer kms alias time to config banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-03 08:28:44 UTC
+# Last sync: 2026-08-03 09:56:26 UTC
