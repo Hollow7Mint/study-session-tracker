dbc95c7bc7879b9032e9865257b566f012174366
James Holloway <james.holloway94@gmail.com>
2026-08-03 11:22:01 +0000

    backfill s3 versioning sync marker in metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-03 10:41:20 UTC
+# Last sync: 2026-08-03 11:22:01 UTC
