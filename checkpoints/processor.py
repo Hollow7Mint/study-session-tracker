300dfb9d7ac441038bafefb4bbe50c468a8b1737
James Holloway <james.holloway94@gmail.com>
2026-08-04 08:00:41 +0000

    unify ssh sync record with metadata heartbeat

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-04 07:04:15 UTC
+# Last sync: 2026-08-04 08:00:41 UTC
