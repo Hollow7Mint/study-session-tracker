03c10b69f37c8ed8d839552a5d6e60cc35e81fec
James Holloway <james.holloway94@gmail.com>
2026-08-05 15:14:47 +0000

    load mysql slow query log sync from pipeline

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-05 14:12:22 UTC
+# Last sync: 2026-08-05 15:14:47 UTC
