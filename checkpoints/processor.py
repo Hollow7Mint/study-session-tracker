c00eec56e4d35fd2697f88ec621b32b7dc7aa36e
James Holloway <james.holloway94@gmail.com>
2026-08-05 00:24:16 +0000

    sync ssm parameter refresh after pipeline pass

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-04 23:33:28 UTC
+# Last sync: 2026-08-05 00:24:16 UTC
