de65b7d0d074edf109509db771faf0c61f8208cf
James Holloway <james.holloway94@gmail.com>
2026-08-05 19:20:48 +0000

    recheck bastion connectivity sync after maintenance

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-05 18:21:34 UTC
+# Last sync: 2026-08-05 19:20:48 UTC
