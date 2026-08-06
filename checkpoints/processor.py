34c6bd1eeef8cf3542c3fee0ffe4ef37f545a61c
James Holloway <james.holloway94@gmail.com>
2026-08-06 12:40:20 +0000

    snapshot mysql instance time for database audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-06 11:50:56 UTC
+# Last sync: 2026-08-06 12:40:20 UTC
