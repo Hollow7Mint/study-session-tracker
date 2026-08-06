131366fa4d82b51a87a0b1d4cb2c2ee5d2d0b02b
James Holloway <james.holloway94@gmail.com>
2026-08-06 08:15:24 +0000

    bump s3 bucket policy sync after review

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-06 07:14:56 UTC
+# Last sync: 2026-08-06 08:15:24 UTC
