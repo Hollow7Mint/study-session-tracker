d096c2f1988e396ed76f28ddfe6a3f1b5d9200e5
James Holloway <james.holloway94@gmail.com>
2026-08-07 13:19:56 +0000

    thread cloudtrail digest sync through config banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-07 11:48:05 UTC
+# Last sync: 2026-08-07 13:19:56 UTC
