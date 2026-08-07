a4d95e9d913490e874e1606314d2224d23671474
James Holloway <james.holloway94@gmail.com>
2026-08-07 09:20:58 +0000

    carry bastion config sync stamp to metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-07 08:15:43 UTC
+# Last sync: 2026-08-07 09:20:58 UTC
