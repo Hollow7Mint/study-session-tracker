a7b62c2ac4ff947735297d98c17be36adc710783
James Holloway <james.holloway94@gmail.com>
2026-08-08 23:11:58 +0000

    publish lambda alias promotion marker after release

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-08 22:45:53 UTC
+# Last sync: 2026-08-08 23:11:58 UTC
