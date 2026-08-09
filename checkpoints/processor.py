ccfcced631ffa664c2e57c52642152c40f1e6e63
James Holloway <james.holloway94@gmail.com>
2026-08-09 04:13:57 +0000

    inject redis eviction sync marker into config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-09 03:31:52 UTC
+# Last sync: 2026-08-09 04:13:57 UTC
