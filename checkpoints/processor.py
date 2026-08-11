2266a2fa150a3b4ba9ad1243c676716592daebde
James Holloway <james.holloway94@gmail.com>
2026-08-11 13:07:48 +0000

    queue ssh agent forwarding sync after config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-11 11:49:19 UTC
+# Last sync: 2026-08-11 13:07:48 UTC
