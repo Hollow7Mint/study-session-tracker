ac73fbbf04dd327eb4c8605d85a3c713e6060219
James Holloway <james.holloway94@gmail.com>
2026-08-12 01:51:44 +0000

    pin bastion ip allowlist time in config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-12 01:02:38 UTC
+# Last sync: 2026-08-12 01:51:44 UTC
