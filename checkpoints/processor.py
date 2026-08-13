7ab9410a37285b30bae83085dabddbb2ccb22512
James Holloway <james.holloway94@gmail.com>
2026-08-13 09:14:03 +0000

    transfer redis node time to cluster banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-13 08:59:18 UTC
+# Last sync: 2026-08-13 09:14:02 UTC
