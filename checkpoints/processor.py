5f094e7ed7bdcb384459d8babe589f7efbe3bd5d
James Holloway <james.holloway94@gmail.com>
2026-08-13 08:05:08 +0000

    prime codepipeline stage heartbeat after approval

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-13 06:13:41 UTC
+# Last sync: 2026-08-13 08:05:08 UTC
