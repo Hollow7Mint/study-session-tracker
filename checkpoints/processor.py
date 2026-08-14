31fc9efd174ebe0a491c8338a83cbe13d2e97c9e
James Holloway <james.holloway94@gmail.com>
2026-08-14 13:21:15 +0000

    seal postgres logical replication sync in banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-14 13:00:24 UTC
+# Last sync: 2026-08-14 13:21:15 UTC
