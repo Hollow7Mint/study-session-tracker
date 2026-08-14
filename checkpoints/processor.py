39ef3a1c9ae8e18f07504304f2c565233e12e8e7
James Holloway <james.holloway94@gmail.com>
2026-08-14 00:07:33 +0000

    emit lambda function deploy heartbeat for release

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-13 22:19:06 UTC
+# Last sync: 2026-08-14 00:07:33 UTC
