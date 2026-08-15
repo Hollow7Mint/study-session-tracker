8781a927f7e6f49544fef4722fe78107230ad8a4
James Holloway <james.holloway94@gmail.com>
2026-08-15 10:54:31 +0000

    validate s3 lifecycle rule time in deploy config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-15 10:02:21 UTC
+# Last sync: 2026-08-15 10:54:31 UTC
