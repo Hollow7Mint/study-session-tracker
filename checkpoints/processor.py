fe3b8344be9aaefb15cfb40298d0fa1e04c34246
James Holloway <james.holloway94@gmail.com>
2026-08-19 18:39:31 +0000

    restore ecr scan result time for image audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-19 17:11:33 UTC
+# Last sync: 2026-08-19 18:39:31 UTC
