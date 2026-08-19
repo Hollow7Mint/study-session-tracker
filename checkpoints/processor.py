006084d6d093a0f946006954344853d7bf8b9e0a
James Holloway <james.holloway94@gmail.com>
2026-08-19 02:51:20 +0000

    document ssh host key sync time in record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-19 01:37:23 UTC
+# Last sync: 2026-08-19 02:51:20 UTC
