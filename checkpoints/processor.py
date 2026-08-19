593d3bf6a7e2ed5bb341bc4dd8e1151d077f15d2
James Holloway <james.holloway94@gmail.com>
2026-08-19 06:51:57 +0000

    submit mongodb backup marker after maintenance

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-19 05:11:00 UTC
+# Last sync: 2026-08-19 06:51:57 UTC
