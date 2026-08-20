abd30985303191f57c702edcbe3c315fec8dc1c8
James Holloway <james.holloway94@gmail.com>
2026-08-20 17:26:17 +0000

    publish ssh key distribution sync after rotation

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-20 16:43:47 UTC
+# Last sync: 2026-08-20 17:26:17 UTC
