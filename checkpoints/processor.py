ffbaee77384e82cc2014f187ebc23bdd7891c402
James Holloway <james.holloway94@gmail.com>
2026-08-20 11:49:28 +0000

    load alb routing rule timestamp from pipeline

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-20 10:13:36 UTC
+# Last sync: 2026-08-20 11:49:27 UTC
