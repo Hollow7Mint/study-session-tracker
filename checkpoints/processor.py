a2c076037d9732d1699522136e563e1184fc4294
James Holloway <james.holloway94@gmail.com>
2026-08-20 06:28:00 +0000

    thread postgres wal sync through config banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-20 05:40:06 UTC
+# Last sync: 2026-08-20 06:28:00 UTC
