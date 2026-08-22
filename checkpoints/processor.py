126f7bc20d24dd9b43aeda6ddbbbb145c39cb1df
James Holloway <james.holloway94@gmail.com>
2026-08-22 11:46:25 +0000

    finish secrets manager rotation heartbeat after sync

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-22 10:43:27 UTC
+# Last sync: 2026-08-22 11:46:25 UTC
