38b499ea942f6ee3a0ed942ce00288a7e2658e71
James Holloway <james.holloway94@gmail.com>
2026-08-22 08:10:47 +0000

    recheck ecs service sync after deployment window

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-22 07:17:55 UTC
+# Last sync: 2026-08-22 08:10:47 UTC
