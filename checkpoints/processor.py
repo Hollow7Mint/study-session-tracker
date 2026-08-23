de5a2ca2c62e419541288848f37de47142899d72
James Holloway <james.holloway94@gmail.com>
2026-08-23 19:19:44 +0000

    retain cloudwatch metric filter time for audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-23 18:47:15 UTC
+# Last sync: 2026-08-23 19:19:44 UTC
