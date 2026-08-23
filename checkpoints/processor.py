4c838f7a6faf3a7278ad999a725f42a03776a5cd
James Holloway <james.holloway94@gmail.com>
2026-08-23 11:59:40 +0000

    save inspector assessment marker after scan window

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-23 10:54:35 UTC
+# Last sync: 2026-08-23 11:59:40 UTC
