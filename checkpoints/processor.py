ea9c2748c013bd2d9a530301dae93ad0a14e0f10
James Holloway <james.holloway94@gmail.com>
2026-08-24 03:24:41 +0000

    log shield ddos event time in protection banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-24 02:13:47 UTC
+# Last sync: 2026-08-24 03:24:41 UTC
