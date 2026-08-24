da93afcbbe0edbcff46faca9b4ffa0db86a7f476
James Holloway <james.holloway94@gmail.com>
2026-08-24 04:33:19 +0000

    relay db connection string time through config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-24 03:24:41 UTC
+# Last sync: 2026-08-24 04:33:19 UTC
