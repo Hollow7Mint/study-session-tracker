3263d4521e7f5e362efd453b38fb120a49a8bc0d
James Holloway <james.holloway94@gmail.com>
2026-08-24 10:20:43 +0000

    catalog mongodb document count sync after audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-24 09:13:12 UTC
+# Last sync: 2026-08-24 10:20:43 UTC
