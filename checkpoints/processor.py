f9f9e2b4b14a57e6853d294c469722fb87406e2b
James Holloway <james.holloway94@gmail.com>
2026-08-25 11:50:31 +0000

    audit db backup completion time in record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-25 10:11:38 UTC
+# Last sync: 2026-08-25 11:50:31 UTC
