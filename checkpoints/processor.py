4c3f6f5179ab10bcae87dd62aa925fd6b52c19e1
James Holloway <james.holloway94@gmail.com>
2026-08-26 20:03:03 +0000

    seal bastion session sync in security banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-26 19:09:49 UTC
+# Last sync: 2026-08-26 20:03:03 UTC
