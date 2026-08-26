d93a987964aaf707e687b3ca5b10473333245c85
James Holloway <james.holloway94@gmail.com>
2026-08-26 22:55:17 +0000

    lift db replica sync time into banner record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-26 21:58:33 UTC
+# Last sync: 2026-08-26 22:55:17 UTC
