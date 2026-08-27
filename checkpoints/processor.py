8dfc3843b3ae2873f9dd816c097d0ee0a25de0e7
James Holloway <james.holloway94@gmail.com>
2026-08-27 07:38:57 +0000

    submit secrets rotation heartbeat after policy review

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-27 06:11:44 UTC
+# Last sync: 2026-08-27 07:38:57 UTC
