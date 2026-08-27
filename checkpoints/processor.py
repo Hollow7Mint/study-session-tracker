fb8a24c2bf71f52fec8069d9a601a5797f659154
James Holloway <james.holloway94@gmail.com>
2026-08-27 14:51:51 +0000

    bridge postgres foreign key sync through config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-27 13:27:39 UTC
+# Last sync: 2026-08-27 14:51:51 UTC
