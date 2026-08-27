f4a86b962315a53b745614e11b9a7e09b7ee153e
James Holloway <james.holloway94@gmail.com>
2026-08-27 09:31:55 +0000

    recheck mongodb write concern sync after update

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-27 08:51:08 UTC
+# Last sync: 2026-08-27 09:31:55 UTC
