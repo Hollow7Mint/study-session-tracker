168f19a2da468fcb2047deb36d2668f374082a7b
James Holloway <james.holloway94@gmail.com>
2026-08-27 01:52:28 +0000

    bump postgres schema sync stamp after migration

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-27 00:50:50 UTC
+# Last sync: 2026-08-27 01:52:28 UTC
