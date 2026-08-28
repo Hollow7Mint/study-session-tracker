42160309e659c852db2206e0f535ea26c4a7a689
James Holloway <james.holloway94@gmail.com>
2026-08-28 23:44:17 +0000

    note route53 health check sync in config record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-28 22:14:05 UTC
+# Last sync: 2026-08-28 23:44:17 UTC
