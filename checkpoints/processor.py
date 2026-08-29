364dbcbac15761d9c0f5cea07f401afb1b1cfd04
James Holloway <james.holloway94@gmail.com>
2026-08-29 04:30:28 +0000

    normalize ssh cipher suite sync time in metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-29 03:31:40 UTC
+# Last sync: 2026-08-29 04:30:27 UTC
