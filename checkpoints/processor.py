9c4ec08e2f614667d08d2902556a3b2283b06cb8
James Holloway <james.holloway94@gmail.com>
2026-08-31 06:53:00 +0000

    run guardduty detector heartbeat for scheduled audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-31 05:33:10 UTC
+# Last sync: 2026-08-31 06:53:00 UTC
