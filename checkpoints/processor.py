0410c66b46c65cfd6fc99603836c5df012e04933
James Holloway <james.holloway94@gmail.com>
2026-09-01 19:52:23 +0000

    reload bastion session recording sync for audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-01 18:11:27 UTC
+# Last sync: 2026-09-01 19:52:23 UTC
