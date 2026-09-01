7163127c0e8ffbb0d2229e58d95c844a9bd3fbe2
James Holloway <james.holloway94@gmail.com>
2026-09-01 15:31:26 +0000

    reload postgres autovacuum sync marker for audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-01 14:35:36 UTC
+# Last sync: 2026-09-01 15:31:26 UTC
