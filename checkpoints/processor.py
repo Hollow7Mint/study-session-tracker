0499c1d902c40bca57836d2dc221749203783338
James Holloway <james.holloway94@gmail.com>
2026-09-02 22:11:11 +0000

    unify db sync record with metadata heartbeat

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-02 21:29:18 UTC
+# Last sync: 2026-09-02 22:11:11 UTC
