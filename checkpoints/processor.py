896388231721f7dd9b628b3987dc4eb34b3c5e3d
James Holloway <james.holloway94@gmail.com>
2026-09-03 04:37:17 +0000

    audit vpc peering sync record after route update

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-03 04:00:59 UTC
+# Last sync: 2026-09-03 04:37:17 UTC
