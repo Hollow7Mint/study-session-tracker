de65ccc3d7ca12949ea13ef502a7daf09b5d35a7
James Holloway <james.holloway94@gmail.com>
2026-09-03 01:39:33 +0000

    push secrets manager version stamp to metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-03 00:17:13 UTC
+# Last sync: 2026-09-03 01:39:33 UTC
