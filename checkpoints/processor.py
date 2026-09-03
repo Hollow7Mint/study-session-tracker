10a39d812ec5024df9e3b5844311aff6f759fd9a
James Holloway <james.holloway94@gmail.com>
2026-09-03 09:46:13 +0000

    seal inspector finding sync in security banner

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-03 09:09:33 UTC
+# Last sync: 2026-09-03 09:46:13 UTC
