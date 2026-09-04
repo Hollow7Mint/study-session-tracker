9ac1c06516f414ff0b594fa2bad188a097ce8fdf
James Holloway <james.holloway94@gmail.com>
2026-09-04 21:16:22 +0000

    frame mysql connection limit sync in config

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-04 20:16:36 UTC
+# Last sync: 2026-09-04 21:16:22 UTC
