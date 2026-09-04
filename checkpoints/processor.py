1a4a375a2b72da0fbaa7181de6305baf532a54ef
James Holloway <james.holloway94@gmail.com>
2026-09-04 17:50:52 +0000

    emit ssh certificate renewal heartbeat for audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-04 16:39:24 UTC
+# Last sync: 2026-09-04 17:50:52 UTC
