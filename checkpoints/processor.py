57887471e61f178c59d1dd0c8206eafe0ca7e55a
James Holloway <james.holloway94@gmail.com>
2026-09-04 06:56:05 +0000

    run mongodb heartbeat for scheduled audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-04 05:12:21 UTC
+# Last sync: 2026-09-04 06:56:05 UTC
