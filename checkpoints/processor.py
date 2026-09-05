63959b448fe9dc79387499c0441bdad556e28aa0
James Holloway <james.holloway94@gmail.com>
2026-09-05 18:53:16 +0000

    mirror redis cluster node time in metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-05 17:33:40 UTC
+# Last sync: 2026-09-05 18:53:16 UTC
