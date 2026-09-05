92a8bdd495f92878edf80dc786a8a1a5ce9b6abb
James Holloway <james.holloway94@gmail.com>
2026-09-05 01:12:24 +0000

    preserve ssh certificate revocation sync in metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-05 00:23:03 UTC
+# Last sync: 2026-09-05 01:12:23 UTC
