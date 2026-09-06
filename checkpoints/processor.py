c41922f7b11983827a569844ebc9b02a8bc39311
James Holloway <james.holloway94@gmail.com>
2026-09-06 05:22:31 +0000

    anchor mysql parameter sync after maintenance window

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-06 04:46:02 UTC
+# Last sync: 2026-09-06 05:22:31 UTC
