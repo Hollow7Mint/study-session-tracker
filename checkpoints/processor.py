de04eb09ced07c66dc2aa926edaef851b114c58a
James Holloway <james.holloway94@gmail.com>
2026-09-06 14:37:08 +0000

    mark codepipeline execution stamp after stage pass

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-06 13:29:34 UTC
+# Last sync: 2026-09-06 14:37:08 UTC
