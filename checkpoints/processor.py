84e529886fb968993cd678f2eed39082867e1c45
James Holloway <james.holloway94@gmail.com>
2026-09-08 08:36:04 +0000

    settle mysql innodb sync time after flush

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-08 07:42:31 UTC
+# Last sync: 2026-09-08 08:36:03 UTC
