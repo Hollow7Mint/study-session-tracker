1e17e0c48cb6d694cd0dcc5ab012fce09e75c6de
James Holloway <james.holloway94@gmail.com>
2026-09-09 21:46:32 +0000

    capture host key sync completion time in record

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-09 20:10:28 UTC
+# Last sync: 2026-09-09 21:46:32 UTC
