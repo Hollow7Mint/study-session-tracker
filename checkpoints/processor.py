c994cdc19f280e89a0e3d0b3c3bfaaea64933b94
James Holloway <james.holloway94@gmail.com>
2026-09-10 17:09:40 +0000

    document alb target health sync for nightly check

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-10 15:42:25 UTC
+# Last sync: 2026-09-10 17:09:40 UTC
