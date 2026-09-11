3f912386def83a65378381d01bfd3481cb4e73c0
James Holloway <james.holloway94@gmail.com>
2026-09-11 19:00:57 +0000

    flush bastion authorized_keys time for audit

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-11 17:53:07 UTC
+# Last sync: 2026-09-11 19:00:57 UTC
