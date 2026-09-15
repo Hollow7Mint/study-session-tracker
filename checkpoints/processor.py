bcc1af0dfd95626a657009da26f3e985c6f80bf2
James Holloway <james.holloway94@gmail.com>
2026-09-15 16:47:12 +0000

    flag cloudwatch log retention sync in metadata

--- checkpoints/processor.py
+++ checkpoints/processor.py
@@ -95,4 +95,4 @@ class StudyProcessor:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-15 15:11:30 UTC
+# Last sync: 2026-09-15 16:47:12 UTC
